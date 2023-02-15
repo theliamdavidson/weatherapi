1aeed3e182346e8f0fde69c049241d86acd0fda8
Liam Davidson <46986875+theliamdavidson@users.noreply.github.com>
2023-02-14 16:41:14 -0800

    inplementing next level

--- weather_api.py
+++ weather_api.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
+import time
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
  
@@ -13,7 +15,18 @@ def weather():
     weather = soup.select('#wob_tm')[0].getText().strip()
     print(info)
     print(weather+"°f")
+    return([info, weather+"°f"])
  
- 
+def send_over_spi(package):
+    print(package)
+    
 
-weather()
+if __name__ == "__main__":
+    t = datetime.datetime.today()
+    future = datetime.datetime(t.year,t.month,t.day,6,45)
+    if t.hour >= 7:
+        future += datetime.timedelta(days=1)
+        print((future-t).total_seconds())
+        
+    time.sleep((future-t).total_seconds())
+    send_over_spi(weather())
